2426f5499c9813b9b833e093aad6fde350d0bb37
Eli7108 <Batipeli000@hsestudents.org>
2026-09-14 08:44:45 -0400

    Mid-September Push
    All recent updates/changes made to each page in the portfolio.
    - Added a dark mode to the portfolio.
    - using a combination of CSS and JS.
    - Added CSS to each page for formatting.
    - Started adding information about a few different coding projects I've done up to now.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
     let text = d.toString();
     document.getElementById("date").innerHTML = text;
 });
+
+function darkModeButton() {
+    var body = document.body;
+    body.classList.toggle("darkMode");
+
+    var sections = document.querySelectorAll("section");
+    sections.forEach(function(section) {
+        section.classList.toggle("darkMode2");
+    });
+
+    var header = document.querySelector("header");
+    header.classList.toggle("darkMode3");
+
+    var navigation = document.querySelector("nav");
+    navigation.classList.toggle("darkMode4");
+
+    var pageLinks = document.querySelectorAll("a");
+    pageLinks.forEach(function(link) {
+        link.classList.toggle("darkMode5");
+    });
+}
